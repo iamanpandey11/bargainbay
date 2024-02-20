38904c9b6442f8835eecd9245760c24a806b5a24
Aman Pandey <paman4681@gmail.com>
2024-02-20 11:13:29 +0530

    err fixed

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -8,6 +8,7 @@ import {
   signInFailure,
 } from "../redux/user/userSlice";
 import OAuth from "../components/OAuth";
+import logo from "../images/logo.jpeg";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -48,10 +49,7 @@ export default function SignIn() {
         {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Sahand's
-            </span>
-            Blog
+            <img src={logo} alt="image" className="h-20" />
           </Link>
           <p className="text-sm mt-5">
             This is a demo project. You can sign in with your email and password
