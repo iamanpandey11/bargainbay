dda6ce8c3302cbedcf389813102809dfe62ccd6c
Aman Pandey <paman4681@gmail.com>
2024-02-26 10:59:58 +0530

    Revert "mobile database connection is done"
    This reverts commit 9640b8426088b7b11e3f1024a9ba79be547e18ee.

--- api/controllers/mobile.controller.js
+++ api/controllers/mobile.controller.js
@@ -7,15 +7,18 @@ export const test = async (req, res, next) => {
 export const getMobile = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
-    const limit = parseInt(req.limit) || 100;
+    const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === "asc" ? 1 : -1;
-    console.log(req.body);
-    const mobiles = await Mobile.find(req.body).skip(startIndex).limit(limit);
+    // const posts = await Mobile.find(...{ brand_name: req.query.brand_name })
+    //   .skip(startIndex)
+    //   .limit(limit);
+    console.log();
+    const posts = await Mobile.find().skip(startIndex).limit(limit);
     const totalPosts = await Mobile.countDocuments();
 
     res.status(200).json([
       {
-        mobiles,
+        posts,
         totalPosts,
       },
     ]);
